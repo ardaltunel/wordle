e23b748120f8c0f29a375f728885f1a9d46be54f
Arda Altunel <ardaltunelmain@gmail.com>
2023-06-15 21:21:54 +0300

    ardaltunel

---
+++ index.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <title>Wordle - Arda Altunel</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+
+<div id="score">Score :</div>
+<div id="game" class="game">
+    <h1>WORDLE</h1>
+    <div id="grid"></div>
+    <div id="message">&nbsp;</div>
+</div>
+
+
+<script src="script.js"></script>
+</body>
+</html>
